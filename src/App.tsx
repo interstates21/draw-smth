bde6a4403de73983e4da661e205b85c737f73772
Oleksii Kupin <interstates21@gmail.com>
2020-06-18 20:05:39 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+
+const Header = () => {
+  const [val, setVal] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setVal(newValue);
+  };
+
+  return (
+    <AppBar position="static" color="default">
+      <Tabs
+        value={val}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        color="secondary"
+        variant="fullWidth"
+        aria-label="full width tabs example"
+      >
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+    </AppBar>
+  );
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#343d46", width: "100vw", height: "100vh" }}>
+      <Header />
     </div>
   );
 }
